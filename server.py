03faa117fcd09caf0c0dd60568c86cab2d780ea6
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-06-21 17:17:40 +0200

    updates

--- server.py
+++ server.py
@@ -1,16 +1,21 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/transcribe", methods=['POST'])
+@app.route('/transcribe', methods=['POST'])
 def transcribe():
-    response = "<p>Hello, World!</p>"
-    return jsonify(response)
+    file = request.files['file']
+    transcription = "Transcribed text"
+    return jsonify({'transcription': transcription})
+
     
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(port=5000)
